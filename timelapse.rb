9a401c59830d854a16f56110144228812e0bd9fe
joycedelatorre <joycedelatorre87@gmail.com>
2015-10-01 11:55:20 -0700

    bug fix and download method merge with take pictures

--- timelapse.rb
+++ timelapse.rb
@@ -6,21 +6,20 @@ require 'json'
 
 $pictures=[]
 def take_picture(num_to_take, num_sleep)
-  picture_container = []
   num_of_pictures = 0
 
+  @urlstring_to_post = 'http://10.0.0.1:10000/sony/camera'
+
+  @sound = HTTParty.post(@urlstring_to_post.to_str,
+    :body => {
+              :method => 'setBeepMode',
+              :params => ['Off'],
+              :id => 1,
+              :version =>'1.0'
+             }.to_json,
+    :headers => {'Content-Type' => 'application/json'})
 
   while num_of_pictures < num_to_take do
-
-    @urlstring_to_post = 'http://10.0.0.1:10000/sony/camera'
-    @sound = HTTParty.post(@urlstring_to_post.to_str,
-        :body => {
-          :method => 'setBeepMode',
-          :params => ['Off'],
-          :id => 1,
-          :version =>'1.0'
-          }.to_json,
-          :headers => {'Content-Type' => 'application/json'})
 
     @result = HTTParty.post(@urlstring_to_post.to_str,
         :body => {
@@ -31,23 +30,18 @@ def take_picture(num_to_take, num_sleep)
                  }.to_json,
         :headers => {'Content-Type' => 'application/json'})
 
-    num_of_pictures += 1
-    picture_container << @result["result"][0][0]
-    sleep(num_sleep)
-    #clean picture_container.
-  end
-  $pictures << picture_container
-end
-
-take_picture(3,0)
-
-def download_pics()
-  $pictures[0].each do |pic|
-    filename_pic = pic.slice(/\bpict\d*\_\d*\.[A-Z]*/)
-    open("pictures/" + filename_pic, 'wb') do |file|
-      file << open(pic).read
-      # p file
+    @result["result"].each do |pic|
+      filename_pic = pic[0].slice(/\bpict\d*\_\d*\.[A-Z]*/)
+      open("pictures/" + filename_pic, 'wb') do |file|
+          file << open(pic[0]).read
+        end
     end
+
+    num_of_pictures += 1
+    sleep(num_sleep)
   end
 end
-download_pics()
+
+take_picture(10,0) # as you take picture it will automatically download the image to the pictures directory.
+
+
